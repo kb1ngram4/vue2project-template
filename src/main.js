af38731cfdbc431121a3f9ed3db4eab4f363874d
kobe_xing <1318947374@qq.com>
2023-03-06 10:46:29 +0800

    安装适合vue2的vant依赖

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import vant from 'vant'
+
+import 'vant/lib/index.css';
+Vue.use(vant)
+
 Vue.config.productionTip = false
 
 new Vue({
